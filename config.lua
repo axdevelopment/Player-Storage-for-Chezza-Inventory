ab69d916344d280c38ed62cfcb2772f1abfa857d
Ardo <90957355+axdevelopment@users.noreply.github.com>
2022-11-28 09:41:42 +0100

    Update config.lua

--- config.lua
+++ config.lua
@@ -5,4 +5,6 @@ Config.Locations = {
 	vector3(2790.9263, 3513.1270, 54.8114)
 }
 
-Config.HelpNotifitcationText = 'Drücke ~INPUT_CONTEXT~ um das Lager zu öffnen'
+Config.HelpNotifitcationText = 'Drücke ~INPUT_CONTEXT~ um das Lager zu öffnen' -- notificationtext
+
+Config.StorageSize = 10000 -- how much storagespace should be in the storages
