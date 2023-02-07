06ecdef11103464d5ba3d2e0480a4226a22c9f01
Ardo <90957355+axdevelopment@users.noreply.github.com>
2023-02-07 10:30:19 +0100

    Update fxmanifest.lua
    version update

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,7 +1,8 @@
 fx_version 'cerulean'
 game 'gta5'
-name 'Spieler Storage x Chezza Inventory'
-author 'Ardo'
+name 'Player Storage x Chezza Inventory'
+author 'Ardo4axdev_'
+version '1.1'
 lua54 'yes'
 client_scripts {
 	'client.lua',
