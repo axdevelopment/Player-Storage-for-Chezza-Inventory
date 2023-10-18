47a6352f716a6b2df685c614a4904c7f9c8b10ca
Ardo <90957355+axdevelopment@users.noreply.github.com>
2023-10-18 12:52:07 +0200

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,24 +1,24 @@
 Config	= {}
 
-Config.HelpNotificationText = 'Press ~INPUT_CONTEXT~ to open your storage'		-- help notification text (you can add your own help notification system on client.lua:22)
+Config.HelpNotificationText = 'Drücke ~INPUT_CONTEXT~ um das Lager zu öffnen'		-- help notification text (you can add your own help notification system on client.lua:22)
 
 Config.StorageLocations = {                                                     	-- add as many storages, as you want
-	vector3(2787.3596, 3515.3413, 54.8345),
-	vector3(2790.9263, 3513.1270, 54.8114)
+	vector3(540.3228, 35.8561, 94.6153), -- lspd
+	vector3(1848.6357, 2582.9009, 45.6723) -- sg
 }
 
 Config.Storage = {																
-	Name				= "Storage",  					-- name & id of the storage
-	Size				= 10000,					-- size of the storages
+	Name				= "Persönliches Lager",  					-- name & id of the storage
+	Size				= 100,					-- size of the storages
 	MarkerType			= 20,						-- marker type https://docs.fivem.net/docs/game-references/markers/
-	MarkerColor 			= {r = 155, g = 0, b = 0, a = 255},		-- color of the marker (rgb), a = alpha (opacity)
+	MarkerColor 			= {r = 150, g = 49, b = 221, a = 255},		-- color of the marker (rgb), a = alpha (opacity)
 	MarkerSize			= {x = 0.5, y = 0.5, z = 0.5},			-- markersize
-	MarkerDrawDistance		= 3.0,						-- marker draw distance
+	MarkerDrawDistance		= 5.0,						-- marker draw distance
 	Blips = {
-		Enabled			= true,
-		Name 			= "Player Storage",                             -- blip name
-		Size 			= 0.7,                                          -- blip size 
-		Sprite 			= 525,                                          -- blip type https://docs.fivem.net/docs/game-references/blips/
+		Enabled			= false,
+		Name 			= "Lager",                                          	-- blip name
+		Size 			= 0.7,                                              	-- blip size 
+		Sprite 			= 525,                                              	-- blip type https://docs.fivem.net/docs/game-references/blips/
 		Color			= 32                                            -- blip color https://docs.fivem.net/docs/game-references/blips/#blip-colors
 	}
 }
